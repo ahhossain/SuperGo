792a635630463a6f73fa095e8cd45be6484e8bb1
ahhossain <hossaina@tcd.ie>
2025-08-23 22:19:58 +0100

    added function to get file size

--- cmd/SuperGo/main.go
+++ cmd/SuperGo/main.go
@@ -7,8 +7,14 @@ import (
 )
 
 func main() {
-	dlLink := "https://sin-speed.hetzner.com/100MB.bin"
-	outputFile := "100MB.bin"
-	stats := downloader.Download(dlLink, outputFile)
-	fmt.Printf("downloaded %d taking %s time\n", stats.Size, stats.Timetaken)
+	dlLink := "https://httpbin.org/bytes/1000000"
+	//outputFile := "100MB.bin"
+	contentLength := downloader.GetHead(dlLink)
+	if contentLength > 0 {
+		fmt.Printf("Content length is: %d bytes\n", contentLength)
+	} else {
+		fmt.Println("Could not determine file size. Server may not support Content-Length header.")
+	}
+	//stats := downloader.Download(dlLink, outputFile)
+	//fmt.Printf("downloaded %d taking %s time\n", stats.Size, stats.Timetaken)
 }
--- internal/downloader/download_handler.go
+++ internal/downloader/download_handler.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -29,6 +30,7 @@ func Download(link string, outputFile string) Stats {
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		fmt.Print("Bad response : $s\n", resp.StatusCode)
+		os.Exit(1)
 	}
 	size, err := io.Copy(out, resp.Body)
 	if err != nil {
@@ -43,4 +45,43 @@ func Download(link string, outputFile string) Stats {
 		Timetaken: elapsedTime,
 	}
 	return stats
+}
+
+func GetLength(link string) int64 {
+	resp, err := http.Head(link)
+	if err != nil {
+		fmt.Printf("Error making get request: %s\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Print("Bad response : $s\n", resp.StatusCode)
+		os.Exit(1)
+	}
+	contentLength := resp.ContentLength
+	return contentLength
+}
+
+func GetHead(link string) int64 {
+	var contentLength int64
+	req, err := http.NewRequest("GET", link, nil)
+	if err != nil {
+		fmt.Printf("Error creating request: %v\n", err)
+	}
+	req.Header.Set("Range", "bytes=0-0")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error making get request: %s\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusPartialContent || resp.StatusCode == http.StatusOK {
+		contentLength, err := strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 64)
+		if err != nil {
+			fmt.Printf("Error reading content-length from header: %v\n", err)
+		}
+		return contentLength
+	}
+	return contentLength
 }
