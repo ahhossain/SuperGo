87b7ffd416b9363f90497d2095c80029e1e5c590
ahhossain <hossaina@tcd.ie>
2025-08-25 23:30:51 +0100

    chunk downloading complete

--- cmd/SuperGo/main.go
+++ cmd/SuperGo/main.go
@@ -2,19 +2,70 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/ahhossain/SuperGo/internal/downloader"
 )
 
 func main() {
-	dlLink := "https://httpbin.org/bytes/1000000"
-	//outputFile := "100MB.bin"
+	dlLink := "https://releases.ubuntu.com/25.04/ubuntu-25.04-desktop-amd64.iso"
+	outputFile := `E:\temp\ubuntu-25.04-desktop-amd64.iso`
 	contentLength := downloader.GetHead(dlLink)
 	if contentLength > 0 {
 		fmt.Printf("Content length is: %d bytes\n", contentLength)
 	} else {
 		fmt.Println("Could not determine file size. Server may not support Content-Length header.")
 	}
-	//stats := downloader.Download(dlLink, outputFile)
-	//fmt.Printf("downloaded %d taking %s time\n", stats.Size, stats.Timetaken)
+	chunks := createChunks(contentLength, downloader.ChunkSize, outputFile)
+	chunkChan := make(chan downloader.Chunk, len(chunks))
+	var wg sync.WaitGroup
+	numWorkers := 12
+	startTime := time.Now()
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1) // Increment the WaitGroup counter for each worker
+		go func() {
+			defer wg.Done()
+			for chunk := range chunkChan {
+				_ = downloader.GetChunk(chunk, dlLink, &wg)
+				fmt.Printf("Chunk %d: Start=%d, End=%d, FileName=%s\n", chunk.ChunkNumber, chunk.StartChunk, chunk.EndChunk, chunk.ChunkName)
+			}
+		}()
+	}
+	for i := 1; i <= len(chunks); i++ {
+		chunkChan <- chunks[i-1]
+	}
+	fmt.Println(len(chunks))
+	close(chunkChan)
+	wg.Wait()
+	endTime := time.Now()
+	elapsedTime := endTime.Sub(startTime)
+	fmt.Printf("Time Taken for full download %s\n", elapsedTime)
+	fmt.Println("All chunks have been downloaded.")
+}
+
+func createChunks(contentLength int64, chunkSize int64, outputFile string) []downloader.Chunk {
+	var chunks []downloader.Chunk
+	totalChunks := contentLength / chunkSize
+	var i int64
+	for i = 1; i <= totalChunks; i++ {
+		tempFileName := fmt.Sprintf("%s.%d", outputFile, i)
+		chunk := downloader.Chunk{
+			ChunkNumber: i,
+			ChunkName:   tempFileName,
+			StartChunk:  chunkSize * (i - 1),
+			EndChunk:    (chunkSize * i) - 1,
+		}
+		chunks = append(chunks, chunk)
+	}
+	tempFileName := fmt.Sprintf("%s.%d", outputFile, totalChunks+1)
+	LastChunk := downloader.Chunk{
+		ChunkNumber: i,
+		ChunkName:   tempFileName,
+		StartChunk:  chunkSize * (i - 1),
+		EndChunk:    contentLength,
+	}
+	chunks = append(chunks, LastChunk)
+	return chunks
+
 }
--- internal/downloader/download_handler.go
+++ internal/downloader/download_handler.go
@@ -6,12 +6,23 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 )
+
+const ChunkSize int64 = 4 * 1024 * 1024
 
 type Stats struct {
 	Size      int64
 	Timetaken time.Duration
+}
+
+type Chunk struct {
+	ChunkNumber int64
+	ChunkName   string
+	StartChunk  int64
+	EndChunk    int64
 }
 
 func Download(link string, outputFile string) Stats {
@@ -63,7 +74,6 @@ func GetLength(link string) int64 {
 }
 
 func GetHead(link string) int64 {
-	var contentLength int64
 	req, err := http.NewRequest("GET", link, nil)
 	if err != nil {
 		fmt.Printf("Error creating request: %v\n", err)
@@ -76,12 +86,62 @@ func GetHead(link string) int64 {
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusPartialContent || resp.StatusCode == http.StatusOK {
-		contentLength, err := strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 64)
-		if err != nil {
-			fmt.Printf("Error reading content-length from header: %v\n", err)
+	if resp.StatusCode == http.StatusPartialContent {
+		contentRange := resp.Header.Get("Content-Range")
+		if contentRange == "" {
+			fmt.Println("Content-Range header missing.")
+			return 0
 		}
-		return contentLength
+		parts := strings.Split(contentRange, "/")
+		if len(parts) != 2 {
+			fmt.Println("Invalid Content-Range header format.")
+			return 0
+		}
+		totalSizeStr := parts[1]
+		totalSize, err := strconv.ParseInt(totalSizeStr, 10, 64)
+		if err != nil {
+			fmt.Printf("Error parsing total size from Content-Range: %v\n", err)
+			return 0
+		}
+		return totalSize
+	} else if resp.StatusCode == http.StatusOK {
+		return resp.ContentLength
 	}
-	return contentLength
+	fmt.Printf("Unexpected status code: %d\n", resp.StatusCode)
+	return 0
+}
+
+func GetChunk(chunk Chunk, link string, wg *sync.WaitGroup) Stats {
+	req, err := http.NewRequest("GET", link, nil)
+	if err != nil {
+		fmt.Printf("Error creating request: %v\n", err)
+	}
+	byteRange := fmt.Sprintf("bytes=%d-%d", chunk.StartChunk, chunk.EndChunk)
+	req.Header.Set("Range", byteRange)
+	client := &http.Client{}
+	startTime := time.Now()
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error making get request: %s\n", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusPartialContent {
+		fmt.Printf("Bad response: %s\n", resp.Status)
+	}
+	out, err := os.Create(chunk.ChunkName)
+	if err != nil {
+		fmt.Printf("Unable to create output file: %s\n", err)
+	}
+	defer out.Close()
+	size, err := io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Printf("Error writing to file: %s\n", err)
+	}
+	endTime := time.Now()
+	elapsedTime := endTime.Sub(startTime)
+	stats := Stats{
+		Size:      size,
+		Timetaken: elapsedTime,
+	}
+	return stats
 }
